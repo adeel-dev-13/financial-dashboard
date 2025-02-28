56c470f928e90f6160fc93dba7df938a0572d752
adeeldev <adeeldev13@gmail.com>
2025-02-28 11:16:03 +0500

    updated layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Layout from './components/commonLayout/Layout/Layout.tsx'
+import Layout from './components/primaryLayout/Layout/Layout.tsx'
 import { Providers } from './redux/provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
