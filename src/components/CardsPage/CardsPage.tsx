e29c2556ce2d8ac846c8dd1ec326fc1565f8081d
adeeldev <adeeldev13@gmail.com>
2025-02-28 11:00:40 +0500

    cards page responsiveness

--- src/components/CardsPage/CardsPage.tsx
+++ src/components/CardsPage/CardsPage.tsx
@@ -18,7 +18,7 @@ const CardsPage: React.FC = () => {
         </button>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="flex flex-wrap gap-1 w-full">
         {cardsData.map((card, index) => (
           <Card
             isPrimary={index % 2 === 0}
